25de0ee40cf7dbfa709bb51d5e02f60572d2fd1f
yanceyleo <developer@yanceyleo.com>
2018-11-27 16:34:00 +0800

    build

---
+++ build/precache-manifest.5a52ed45cbd1170610022578df9f3b4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be6fc81846ea2a44b297",
+    "url": "/static/js/main.be6fc818.chunk.js"
+  },
+  {
+    "revision": "b03b36984d6414988055",
+    "url": "/static/js/1.b03b3698.chunk.js"
+  },
+  {
+    "revision": "be6fc81846ea2a44b297",
+    "url": "/static/css/main.1f4de0b2.chunk.css"
+  },
+  {
+    "revision": "ffdbfdbb8aadaef85252c166f9a70b40",
+    "url": "/index.html"
+  }
+];
