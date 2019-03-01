9369f3b0ed8a0355e23cc97b5af4a857e14e70e2
yanceyleo <developer@yanceyleo.com>
2019-03-01 16:35:29 +0800

    添加前端搜索

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
-import { Icon, Button, Table } from 'semantic-ui-react';
+import { Icon, Button, Table, Input } from 'semantic-ui-react';
 import { formatJSONDate, sizeFormat } from '../../tools/tools';
 
 class Content extends Component {
@@ -8,15 +8,17 @@ class Content extends Component {
     super(props);
     this.state = {
       column: null,
-      direction: null
+      direction: null,
+      inputText: ''
     };
   }
 
-  componentWillMount() {}
-
-  componentDidMount() {}
-
-  componentWillUnmount() {}
+  onSearchChange = e => {
+    const input = e.target.value.toLowerCase();
+    this.setState({
+      inputText: input
+    });
+  };
 
   handleSort = clickedColumn => () => {
     const { transformDataSource, dataSource } = this.props;
@@ -42,7 +44,7 @@ class Content extends Component {
 
   render() {
     const { dataSource } = this.props;
-    const { column, direction } = this.state;
+    const { column, direction, inputText } = this.state;
     const styles = {
       table: {
         width: '98%',
@@ -55,121 +57,145 @@ class Content extends Component {
       },
       noDemo: {
         cursor: 'not-allowed'
+      },
+      input: {
+        marginLeft: '1%',
+        marginBottom: '1%',
+        width: '300px'
       }
     };
     return (
-      <Table celled selectable style={styles.table} inverted sortable>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell
-              sorted={column === 'name' ? direction : null}
-              onClick={this.handleSort('name')}
-            >
-              <Icon name="briefcase" />
-              Name
-            </Table.HeaderCell>
-            <Table.HeaderCell collapsing width={4}>
-              <Icon name="info" />
-              Introduction
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={column === 'created_at' ? direction : null}
-              onClick={this.handleSort('created_at')}
-            >
-              <Icon name="clock" />
-              Created Date
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={column === 'updated_at' ? direction : null}
-              onClick={this.handleSort('updated_at')}
-            >
-              <Icon name="arrow circle up" />
-              Updated Date
-            </Table.HeaderCell>
-            <Table.HeaderCell>
-              <Icon name="linkify" />
-              Link
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={column === 'size' ? direction : null}
-              onClick={this.handleSort('size')}
-            >
-              <Icon name="database" />
-              Size
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={column === 'stargazers_count' ? direction : null}
-              onClick={this.handleSort('stargazers_count')}
-            >
-              <Icon name="star" />
-              Stargazers Count
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={column === 'forks_count' ? direction : null}
-              onClick={this.handleSort('forks_count')}
-            >
-              <Icon name="fork" />
-              Forks Count
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={column === 'fork' ? direction : null}
-              onClick={this.handleSort('fork')}
-            >
-              <Icon name="cloud download" />
-              Is Fork?
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {dataSource.map((value, key) => (
-            <Table.Row key={key}>
-              <Table.Cell style={{ textAlign: 'left' }}>
-                {value.name}
-              </Table.Cell>
-              <Table.Cell style={{ textAlign: 'left' }}>
-                {value.description}
-              </Table.Cell>
-              <Table.Cell>
-                {formatJSONDate(value.created_at)}
-              </Table.Cell>
-              <Table.Cell>
-                {formatJSONDate(value.updated_at)}
-              </Table.Cell>
-              <Table.Cell>
-                <Button.Group
-                  style={!value.homepage ? styles.noDemo : null}
-                >
-                  <Button primary>
-                    <a
-                      href={value.html_url}
-                      style={styles.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      GitHub
-                    </a>
-                  </Button>
-                  <Button.Or />
-                  <Button positive disabled={!value.homepage}>
-                    <a
-                      href={value.homepage}
-                      style={styles.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Demo
-                    </a>
-                  </Button>
-                </Button.Group>
-              </Table.Cell>
-              <Table.Cell>{sizeFormat(value.size)}</Table.Cell>
-              <Table.Cell>{value.stargazers_count}</Table.Cell>
-              <Table.Cell>{value.forks_count}</Table.Cell>
-              <Table.Cell>{value.fork ? 'Yes' : 'No'}</Table.Cell>
+      <>
+        <Input
+          style={styles.input}
+          icon="search"
+          iconPosition="left"
+          placeholder="Search..."
+          onChange={e => this.onSearchChange(e)}
+        />
+        <Table celled selectable style={styles.table} inverted sortable>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell
+                sorted={column === 'name' ? direction : null}
+                onClick={this.handleSort('name')}
+              >
+                <Icon name="briefcase" />
+                Name
+              </Table.HeaderCell>
+              <Table.HeaderCell collapsing width={4}>
+                <Icon name="info" />
+                Introduction
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'created_at' ? direction : null}
+                onClick={this.handleSort('created_at')}
+              >
+                <Icon name="clock" />
+                Created Date
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'updated_at' ? direction : null}
+                onClick={this.handleSort('updated_at')}
+              >
+                <Icon name="arrow circle up" />
+                Updated Date
+              </Table.HeaderCell>
+              <Table.HeaderCell>
+                <Icon name="linkify" />
+                Link
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'size' ? direction : null}
+                onClick={this.handleSort('size')}
+              >
+                <Icon name="database" />
+                Size
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'stargazers_count' ? direction : null}
+                onClick={this.handleSort('stargazers_count')}
+              >
+                <Icon name="star" />
+                Stargazers Count
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'forks_count' ? direction : null}
+                onClick={this.handleSort('forks_count')}
+              >
+                <Icon name="fork" />
+                Forks Count
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'fork' ? direction : null}
+                onClick={this.handleSort('fork')}
+              >
+                <Icon name="cloud download" />
+                Is Fork ?
+              </Table.HeaderCell>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+          </Table.Header>
+          <Table.Body>
+            {dataSource
+              .filter(
+                data =>
+                  !inputText ||
+                  data.name.toLowerCase().includes(inputText.toLowerCase())
+              )
+              .map((value, key) => (
+                <Table.Row key={key}>
+                  <Table.Cell
+                    style={{
+                      textAlign: 'left'
+                    }}
+                  >
+                    {value.name}
+                  </Table.Cell>
+                  <Table.Cell
+                    style={{
+                      textAlign: 'left'
+                    }}
+                  >
+                    {value.description}
+                  </Table.Cell>
+                  <Table.Cell> {formatJSONDate(value.created_at)} </Table.Cell>
+                  <Table.Cell> {formatJSONDate(value.updated_at)} </Table.Cell>
+                  <Table.Cell>
+                    <Button.Group
+                      style={!value.homepage ? styles.noDemo : null}
+                    >
+                      <Button primary>
+                        <a
+                          href={value.html_url}
+                          style={styles.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          GitHub
+                        </a>
+                      </Button>
+                      <Button.Or />
+                      <Button positive disabled={!value.homepage}>
+                        <a
+                          href={value.homepage}
+                          style={styles.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Demo
+                        </a>
+                      </Button>
+                    </Button.Group>
+                  </Table.Cell>
+                  <Table.Cell> {sizeFormat(value.size)} </Table.Cell>
+                  <Table.Cell> {value.stargazers_count} </Table.Cell>
+                  <Table.Cell> {value.forks_count} </Table.Cell>
+                  <Table.Cell> {value.fork ? 'Yes' : 'No'} </Table.Cell>
+                </Table.Row>
+              ))}
+          </Table.Body>
+        </Table>
+      </>
     );
   }
 }
